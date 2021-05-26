73ae6ac5f40758b9182b6fd5b3fc556beeb9f44c
tfh <tfh@mit.edu>
2021-05-26 16:54:38 -0400

    Adding comments to page

--- js/our.js
+++ js/our.js
@@ -1,10 +1,30 @@
-function Our() {
-  //TODO
-  //this.address = window.location.href;
-  //this.http = new XMLHttpRequest();
-  //ws = new WebSocket("ws://" + address);
+class OurMentions extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Create a shadow root
+    this.shadow = this.attachShadow({mode: 'open'});
+  }
+
+  connectedCallback() {
+    if(this.hasAttribute('addr')) {
+      let addr = this.getAttribute('addr');
+      this.ws = new WebSocket("ws://localhost:5000/" + addr);
+      this.ws.onmessage = this.onMessage.bind(this);
+    }
+  }
+
+  async onMessage(e) {
+    // Create a child element
+    let response = await fetch("http://localhost:5000/" + e.data);
+    if (response.ok) {
+      let mention = document.createElement('div');
+      mention.setAttribute('class','mention');
+      mention.innerHTML = await response.text();
+      this.shadow.appendChild(mention);
+    }
+  }
 }
 
-Our.prototype.hello = function() {
-  return "Hello, world!";
-}
+customElements.define("our-mentions", OurMentions);
