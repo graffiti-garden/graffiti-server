8d6fb2c07d0ba3b4a0a2ae591f72e9c2bca38309
anonymous <anonymous@example.com>
2021-10-12 10:40:19 -0400

    Code seperated out into multiple files

---
+++ app/attend.py
@@ -0,0 +1,80 @@
+import asyncio
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+from .config import ATTEND_INTERVAL, open_redis
+
+router = APIRouter()
+
+@router.websocket("/attend")
+async def attend(ws: WebSocket):
+    # Accept and create object
+    await ws.accept()
+    at = Attend()
+
+    # Listen for updates
+    while True:
+        try:
+            msg = await ws.receive_json()
+        except WebSocketDisconnect:
+            break
+
+        # Send it to the object
+        await at.receive(ws, msg)
+
+
+class Attend:
+
+    def __init__(self):
+        self.attending = {}
+        self.task  = None
+
+    async def receive(self, ws: WebSocket, msg):
+        # Kill the task if it exists
+        if self.task: self.task.cancel()
+
+        # Add any new stages to attend to
+        for stage in msg.get('add', []):
+            if not stage: continue
+            key = 'stg' + stage
+            if key not in self.attending:
+                self.attending[key] = '0'
+
+        # And remove stage
+        for stage in msg.get('rem', []):
+            if not stage: continue
+            self.attending.pop('stg' + stage, None)
+
+        # Return the attending list as acknowledgment
+        ack = {'attending': [stage[3:] for stage in self.attending.keys()]}
+        await ws.send_json(ack)
+
+        # Start a background listening task if non-empty
+        if self.attending:
+            self.task = asyncio.create_task(self.attend(ws))
+        else:
+            self.task = None
+
+    async def attend(self, ws: WebSocket):
+        # Connect to the database
+        r = await open_redis()
+
+        while True:
+            # Wait for new events
+            events = await r.xread(self.attending,
+                                   block=ATTEND_INTERVAL)
+
+            # Extract the URLs
+            actions = {}
+            for stage, stageevents in events:
+                stage = stage.decode()[3:]
+                actions[stage] = []
+                for id_, event in stageevents:
+                    self.attending['stg' + stage] = id_
+                    actions[stage].append(event[b'act'].decode())
+
+            # Send the output
+            obs = {'observed': actions}
+            try:
+                await ws.send_json(obs)
+            except:
+                break
---
+++ app/perform.py
@@ -0,0 +1,74 @@
+import hashlib
+from fastapi import APIRouter, Depends
+from .login import token_to_user
+from .config import open_redis
+
+router = APIRouter()
+
+def stage_action_to_uuid(stage: str, action: str):
+    # Concatenate the hashes of the stage and action
+    # This prevents trickiness where
+    # stage+action is ambiguous
+    stage_hash  = hashlib.sha256( stage.encode()).digest()
+    action_hash = hashlib.sha256(action.encode()).digest()
+    return stage_hash + action_hash
+
+@router.post('/perform')
+async def perform(
+        stage: str, action: str,
+        user = Depends(token_to_user)):
+
+    # Get a unique performance ID that
+    # describes the stage/action pair
+    per = stage_action_to_uuid(stage, action)
+
+    # Connect to the database and lock
+    r = await open_redis()
+    lock = r.lock(b'loc' + per)
+    await lock.acquire()
+
+    # If no user is already performing
+    if await r.scard(b'per' + per) == 0:
+        # Add it to the stream
+        xid = await r.xadd('stg' + stage, {'act': action})
+        # Map the performance to its
+        # stream ID so it can be deleted.
+        await r.hset(b'xid' + per, 'xid', xid)
+
+    # Add the user to the performance
+    await r.sadd(b'per' + per, user)
+
+    # Release
+    await lock.release()
+    return "Success"
+
+@router.post('/retire')
+async def retire(
+        stage: str, action: str,
+        user = Depends(token_to_user)):
+
+    # Get a unique performance id that
+    # describes the stage/action pair
+    per = stage_action_to_uuid(stage, action)
+
+    # Connect to the database and lock
+    r = await open_redis()
+    lock = r.lock(b'loc' + per)
+    await lock.acquire()
+
+    # Remove the user from the performance
+    await r.srem(b'per' + per, user)
+
+    # If no user is performing
+    # remove the performance from the stream
+    if await r.scard(b'per' + per) == 0:
+        # Get the stream ID of the performance
+        xid = await r.hget(b'xid' + per, 'xid')
+        if xid:
+            # Remove it from the stream
+            await r.xdel('stg' + stage, xid)
+            await r.hdel(b'xid' + per, 'xid')
+
+    # Release
+    await lock.release()
+    return "Success"
