e129264b05a9f05a0d53ee93122744a9d44d28a3
theia <theia@mit.edu>
2021-10-20 00:11:58 -0400

    Login flow through seperate window

--- app/login.py
+++ app/login.py
@@ -1,5 +1,4 @@
 import time
-import string
 import jwt
 from hashlib import sha256
 from os import getenv
@@ -7,52 +6,71 @@ from aiosmtplib import send as sendEmail
 from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formatdate, make_msgid
-from fastapi import APIRouter, Depends, Form, HTTPException
+from fastapi import APIRouter, Depends, Form, HTTPException, Request
 from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2AuthorizationCodeBearer
 
+# TODO:
+# - encrypt email (https://stackoverflow.com/questions/27335726/how-do-i-encrypt-and-decrypt-a-string-in-python)
+# - make a real secret (random every time)
+# - Token expiration
+# - Refresh tokens
+# - Scopes
+# - Magic link
+# - CORS
+
 mail_from = getenv('MAIL_FROM')
-# TODO: clean these up and hide secret
 secret = "secret" 
-code_size = 6
-code_expiration_time = 5 # minutes
+expiration_time = 5 # minutes
 
 router = APIRouter()
 oauth2_scheme = OAuth2AuthorizationCodeBearer(
-        authorizationUrl = "email_code",
+        authorizationUrl = "login",
         tokenUrl = "token")
 
-def generate_code(email: str, minute_offset: int = 0):
+@router.get("/login", response_class=HTMLResponse)
+async def login(
+        client_id: str,
+        redirect_uri: str,
+        state: str):
+    return f"""
+    <script>
+        const email = prompt("Enter your email to be sent a login code:")
+        fetch(`https://theater.csail.mit.edu/email_code?client_id={client_id}&email=${{email}}`, {{method: 'Post'}})
+        .then(response => {{
+            const code = prompt("Enter your login code:")
+            window.location.replace(`{redirect_uri}?state={state}&code=${{code}}`)
+        }})
+    </script>
+    """
 
-    # Each minute has a new code
-    time_in_minutes = int(time.time()/60)
-    time_in_minutes -= minute_offset
+@router.get("/login_redirect", response_class=HTMLResponse)
+async def login_redirect(state: str, code: str):
+    return f"""
+    <script>
+        window.opener.postMessage("{code}", "{state}")
+        window.close()
+    </script>
+    """
 
-    # Compute a secret hash
-    code_data = email + str(time_in_minutes) + secret
-    code_hex = sha256(code_data.encode()).hexdigest()
+@router.post("/email_code")
+async def email_magic(client_id: str, email: str, request: Request):
+    # Make sure the request is being called from another theater page
+    # request.origin
 
-    # Convert the low bits to alpha-numeric
-    alphabet = string.digits + string.ascii_uppercase
-    code = ""
-    code_int = int(code_hex, 16)
-    for _ in range(code_size):
-        code_int, digit = divmod(code_int, len(alphabet))
-        code += alphabet[digit]
-
-    return code
-
-@router.get("/email_code", response_class=HTMLResponse)
-async def email_code(client_id: str, redirect_uri: str, state: str):
-
-    # Make a client-specific and perishable code
-    code = generate_code(client_id)
+    # Construct a code
+    code = jwt.encode({
+        "type": "code",
+        "client_id": client_id,
+        "email": email,
+        "time": time.time()
+        }, secret, algorithm="HS256")
 
     # Put the code in an email
     message = MIMEText(code)
     message["Subject"] = Header("Login Code")
     message["From"] = mail_from
-    message["To"] = client_id
+    message["To"] = email
     message["Message-ID"] = make_msgid()
     message["Date"] = formatdate()
 
@@ -62,65 +80,49 @@ async def email_code(client_id: str, redirect_uri: str, state: str):
     except:
         raise HTTPException(status_code=422, detail="Invalid email.")
 
-    return f"""
-    <form action={redirect_uri}>
-        <input type="hidden" name="state" value="{state}">
-        <label for="code">Enter the login code emailed to {client_id}:</label>
-        <br>
-        <input type="text" name="code" id="code">
-        <br>
-        <button>Submit</button>
-    </form>
-    """
+    return "Success"
 
 @router.post("/token")
-async def token(client_id: str = Form(...), code: str = Form(...)):
+async def token(
+        client_id: str = Form(...),
+        code:      str = Form(...),
+        client_secret: str = Form(...)):
 
-    # Iterate over possible codes generated in
-    # the expiration window and look for a match
-    time_in_minutes = int(time.time()/60)
-    for i in range(0, code_expiration_time):
-        possible_code = generate_code(client_id, i)
-        if code == possible_code:
-            break
-    else:
-        raise HTTPException(status_code=401, detail="Invalid code.")
+    # Assert that the code is valid
+    try:
+        code = jwt.decode(code, secret, algorithms=["HS256"])
+    except:
+        raise HTTPException(status_code=400, detail="Malformed code.")
+    if not code["type"] == "code":
+        raise HTTPException(status_code=400, detail="Malformed code.")
+
+    # Assert that the code has not expired
+    if code["time"] + expiration_time*60 < time.time():
+        raise HTTPException(status_code=400, detail="Expired code.")
+
+    # Assert the client_id is paired to the token
+    if client_id != code["client_id"]:
+        raise HTTPException(status_code=400, detail="Client ID does not match code.")
+
+    # Assert that the secret is valid
+    if sha256(client_secret.encode()).hexdigest() != client_id:
+        raise HTTPException(status_code=400, detail="Invalid client secret.")
     
     # If authorized, create a token
     token = jwt.encode({
+        "type": "token",
         "email": client_id,
         }, secret, algorithm="HS256")
-
-    # TODO: make this token expire and add a refresh token
 
     return {"access_token": token, "token_type": "bearer"}
 
 async def token_to_user(token: str = Depends(oauth2_scheme)):
-
+    # Assert that the token is valid
     try:
-        data = jwt.decode(token, secret, algorithms=["HS256"])
+        token = jwt.decode(token, secret, algorithms=["HS256"])
     except:
         raise HTTPException(status_code=400, detail="Malformed token")
+    if not token["type"] == "token":
+        raise HTTPException(status_code=400, detail="Malformed token.")
 
-    return data["email"]
-
-# TODO:
-
-# In the javascript library you login via a prompt.
-#
-#
-# Optional[This actions requires you to login to.]
-#
-# Enter your email to be sent a login code:
-# [Box]
-# 
-# [Ok], [Cancel]
-#
-#
-# Then another prompt:
-#
-#
-# Enter your login code:
-# [Box]
-#
-# [Ok], [Cancel]
+    return token["email"]
