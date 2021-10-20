2a479fa9831784f3397915956c1812a0e4b4b625
anonymous <anonymous@example.com>
2021-10-20 13:26:53 -0400

    Added cross origin - works on other domains!

--- app/main.py
+++ app/main.py
@@ -3,14 +3,25 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# Allow cross-origin requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+# Serve the API
 routes = ['perform', 'attend', 'pod', 'login']
 for r in routes:
     module = __import__('theater.' + r, fromlist=['router'])
     app.include_router(module.router)
 
+# Serve the static files
 app.mount('/', StaticFiles(directory='www', html=True))
 
 if __name__ == "__main__":
