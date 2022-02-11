acaf4269cf56bdee7b4c624df72bf0000ec03ca4
theia <theia@mit.edu>
2022-02-10 21:01:29 -0500

    multiple query streams through the same socket

--- app/query.py
+++ app/query.py
@@ -1,6 +1,7 @@
 import json
 import asyncio
 import bson
+from uuid import uuid4
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from .auth import token_to_user
@@ -13,67 +14,137 @@ client.get_io_loop = asyncio.get_running_loop
 db = client.test
 
 @router.websocket("/query")
-async def query(ws: WebSocket, token: str, query: str):
+async def query(ws: WebSocket, token: str):
 
     # Validate and convert the token to a user id
     user = token_to_user(token)
 
-    # Make sure the query is valid json
-    try:
-        query = json.loads(query)
-    except json.JSONDecodeError:
-        raise HTTPException(status_code=400, detail=f"Invalid JSON: {query}")
-    if not isinstance(query, dict):
-        raise HTTPException(status_code=400, detail=f"JSON root is not a dictionary: {query}")
+    # Create a query object
+    q = Query(user, ws)
 
-    # Accept the connection
-    await ws.accept()
+    # Wait until it dies
+    await q.heartbeat()
 
-    # Construct a modified query
-    pipeline = [
-        { "$match": {
-            # The activity must match the query
-            "fullDocument.activity": { "$elemMatch": query },
-            # The near misses must NOT match the query
-            "fullDocument.near_misses": { "$not": { "$elemMatch": query } },
-            # The user must be the author, have access, or access must be open
-            "$or": [
-                { "fullDocument.activity.signed": user },
-                { "fullDocument.access": user },
-                { "fullDocument.access": None }
-            ]
-        }}
-    ]
 
-    # Start time at the beginning
-    start_watch_time = bson.timestamp.Timestamp(0, 1)
+class Query:
 
-    # Watch for activities that match the pipeline
-    while True:
-        async with db.activities.watch(
+    def __init__(self, user, ws):
+        self.user = user
+        self.ws = ws
+        self.tasks = {}
+
+
+    async def heartbeat(self):
+        # Accept the connection
+        await self.ws.accept()
+
+        # Listen for messages
+        self.tasks['listener'] = asyncio.create_task(self.listener())
+
+        # Send a heartbeat
+        while True:
+            try:
+                await self.ws.send_json("ping")
+            except WebSocketDisconnect:
+                self.cancel()
+                break
+            await asyncio.sleep(2)
+
+
+    async def listener(self):
+        # Listen for updates
+        while True:
+            try:
+                msg = await self.ws.receive_text()
+            except WebSocketDisconnect:
+                self.cancel()
+                break
+            await self.receive(msg)
+
+
+    async def cancel(self):
+        # Kill all tasks
+        if task in self.tasks.values():
+            task.cancel()
+
+
+    async def receive(self,  msg: str):
+        # Make sure the message is valid json
+        try:
+            msg = json.loads(msg)
+        except json.JSONDecodeError:
+            return await self.error(f"Message is not JSON: {msg}")
+        if not isinstance(msg, dict):
+            return await self.error(f"Message is not an object: {msg}")
+
+        # Parse what action it is performing
+        if not 'action' in msg:
+            return await self.error(f"Message does not specify an action: {msg}")
+        if msg['action'] == 'add':
+            return await self.add_query(msg['query'])
+        elif msg['action'] == 'delete':
+            return await self.del_query(msg['query_id'])
+        else:
+            return await self.error(f"Unrecognized action type in message: {msg}")
+
+
+    async def add_query(self, query):
+        # Make sure the query is valid
+        if not isinstance(query, dict):
+            return await self.error(f"Query is not an object: {query}")
+
+        # Give the query a random ID
+        query_id = str(uuid4())
+
+        # Construct a modified query
+        pipeline = [
+            { "$match": {
+                # The activity must match the query
+                "fullDocument.activity": { "$elemMatch": query },
+                # The near misses must NOT match the query
+                "fullDocument.near_misses": { "$not": { "$elemMatch": query } },
+                # The user must be the author, have access, or access must be open
+                "$or": [
+                    { "fullDocument.activity.signed": self.user },
+                    { "fullDocument.access": self.user },
+                    { "fullDocument.access": None }
+                ]
+            }}
+        ]
+
+        # Construct an iterator
+        change_stream = db.activities.watch(
                 pipeline,
                 full_document='updateLookup',
-                start_at_operation_time=start_watch_time
-                ) as change_stream:
+                start_at_operation_time=bson.timestamp.Timestamp(0, 1)
+                )
 
-            change_stream_iter = change_stream.__aiter__()
-            try:
-                while True:
-                    # Don't wait too long before moving on
-                    change = await asyncio.wait_for(
-                            change_stream_iter.__anext__(),
-                            timeout=2)
+        async def task():
+            async with change_stream as cs:
+                async for change in cs:
+                    await self.ws.send_json({
+                        'query_id': query_id,
+                        'activity': change['fullDocument']['activity'][0]
+                    })
 
-                    # Next iteration of watch, look for posts after this time
-                    current_time = change['clusterTime']
-                    start_watch_time = bson.timestamp.Timestamp(current_time.time, current_time.inc+1)
+        self.tasks[query_id] = asyncio.create_task(task())
 
-                    # Send the message
-                    await ws.send_json(change['fullDocument']['activity'][0])
+        # Send back an acknowledgment that the query has been added
+        await self.ws.send_json({'type': 'QueryAdded', 'query_id': query_id, 'query': query})
 
-            except asyncio.TimeoutError as e:
-                # If we timed out, send a ping just
-                # to make sure the connection is still alive
-                await ws.send_json("I timed out, try again")
 
-    await ws.close()
+    async def del_query(self, query_id):
+        # Make sure the query_id exists
+        if query_id not in self.tasks:
+            self.error(f"Query is not running: {query_id}")
+
+        # Cancel and delete
+        self.tasks[query_id].cancel()
+        del self.tasks[query_id]
+
+        # Send an acknowledgment
+        await self.ws.send_json({'type': 'QueryDeleted', 'query_id': query_id})
+
+
+    async def error(self, detail):
+        await self.ws.send_json({'type': 'Error', 'content': detail})
