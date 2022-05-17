0b17f5cf31cdce542097ef26b2340964dbbb9146
theia <theia@mit.edu>
2022-05-17 10:55:41 -0400

    made magic linking less loopy

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,6 +1,5 @@
 import time
 import jwt
-import base64
 import asyncio
 from hashlib import sha256
 from os import getenv
@@ -8,29 +7,29 @@ from aiosmtplib import send as sendEmail
 from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formatdate, make_msgid
-from typing import Optional
 from fastapi import APIRouter, Form, HTTPException, Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from uuid import uuid5, NAMESPACE_DNS
 
 debug = (getenv('DEBUG') == 'true')
-expiration_time = float(getenv('AUTH_CODE_EXP_TIME')) # minutes
+expiration_time = 60*float(getenv('AUTH_CODE_EXP_TIME')) # min -> sec
 secret = getenv('AUTH_SECRET')
-heartbeat_interval = float(getenv('AUTH_SOCKET_HEARTBEAT'))
-signature_cache_size = int(getenv('AUTH_SIGNATURE_CACHE_SIZE'))
 secret_namespace = uuid5(NAMESPACE_DNS, secret)
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="graffiti/auth/templates")
 
+# For magic linking
+magic_events = {} # hash -> (event, signature)
+
 @router.get("/auth", response_class=HTMLResponse)
 async def auth(
         client_id: str,
         redirect_uri: str,
         request: Request,
-        state: Optional[str] = ""):
+        state: str|None = ""):
 
     # Determine which site is asking for access
     if 'referer' in request.headers:
@@ -102,6 +101,16 @@ async def email(
                 'email': email
             })
 
+    # Create an event
+    now = time.time()
+    signature_hash = sha256(signature.encode()).hexdigest()
+    magic_events[signature_hash] = (asyncio.Event(), signature, now)
+
+    # Remove expired events
+    for sh in magic_events:
+        if magic_events[sh][2] + expiration_time < now:
+            del magic_events[sh]
+
     # Return a form that will combine the code pieces
     # and then send it to the redirect_uri
     return templates.TemplateResponse("code.html", {
@@ -110,7 +119,7 @@ async def email(
         'redirect_uri': redirect_uri,
         'state': state,
         'code_body': header + '.' + payload,
-        'signature_hash': sha256(signature.encode()).hexdigest()
+        'signature_hash': signature_hash
     })
 
 @router.post("/token")
@@ -128,7 +137,7 @@ def token(
         raise HTTPException(status_code=400, detail="Malformed code.")
 
     # Assert that the code has not expired
-    if code["time"] + expiration_time*60 < time.time():
+    if code["time"] + expiration_time < time.time():
         raise HTTPException(status_code=400, detail="Code has expired.")
 
     # Assert that the client_id is paired to the token
@@ -147,42 +156,41 @@ def token(
 
     return {"access_token": token, "owner_id": code["owner_id"], "token_type": "bearer"}
 
-# Below: sockets for magic linking 
-
-hash_to_signature = {}
-
 @router.websocket("/auth_socket")
 async def auth_socket(ws: WebSocket, signature_hash: str):
     await ws.accept()
 
-    # Keep alive
-    while True:
-        try:
-            if signature_hash in hash_to_signature:
-                await ws.send_json({
-                    'type': 'Signature',
-                    'signature': hash_to_signature[signature_hash]
-                })
-                del hash_to_signature[signature_hash]
-                await ws.close()
-                break
-            else:
-                await ws.send_json({'type': 'Ping'})
-        except:
-            break
-        await asyncio.sleep(heartbeat_interval)
+    if signature_hash not in signature_hash_events:
+        await ws.send_json({
+            'type': 'error',
+            'detail': 'hash does not exist.'
+        })
+        await ws.close()
+        return
+
+    # Wait for the event
+    event, signature = magic_events[signature_hash]
+    await event.wait()
+
+    # Send the signature and cleanup
+    await ws.send_json({
+        'type': 'signature',
+        'signature': signature
+    })
+    await ws.close()
 
 @router.get("/auth_socket_send", response_class=HTMLResponse)
 async def auth_socket_send(signature: str):
     # Take the hash of the signature, to make sure it's real
     signature_hash = sha256(signature.encode()).hexdigest()
 
-    # Confirm the hash
-    hash_to_signature[signature_hash] = signature
+    if signature_hash not in magic_events:
+        raise HTTPException(status_code=400, detail="invalid signature hash.")
 
-    # If we have too many hashes, delete an old one
-    # (this could be a bug if too many people try to log in at exactly the same time)
-    if len(hash_to_signature) > signature_cache_size:
-        hash_to_signature.pop(next(iter(hash_to_signature)))
+    # Set the event
+    event, signature = magic_events[signature_hash]
+    event.set()
 
+    # Cleanup and close
+    del magic_events[signature_hash]
     return "<script>window.close()</script>"
